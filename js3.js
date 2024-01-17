df86c6a8ba0090979d1f2d12775dc556136d97bc
sssshahrukh <156694010+sssshahrukh@users.noreply.github.com>
2024-01-17 07:02:55 +0000

    conversion datatype

---
+++ js3.js
@@ -0,0 +1,20 @@
+let score = "33"
+
+// console.log(typeof score)
+// console.log(typeof(score));
+
+let stringnum = Number(score)
+// console.log(typeof stringnum)
+
+
+let check = true
+
+// console.log(typeof check);
+
+let loged = "shahrukh"
+let booleanloged = Boolean(loged)
+console.log(typeof(booleanloged));
+
+// 1 =>true; 0 => false
+// "" => false
+// "shahrukh"=> true
